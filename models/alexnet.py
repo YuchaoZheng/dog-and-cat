f5e283ef8859f644588717403b191f36fbaa0619
YuchaoZheng <yuchao_zheng@163.com>
2018-11-06 19:25:17 +0800

    update alexnet

--- models/alexnet.py
+++ models/alexnet.py
@@ -14,34 +14,14 @@ class AlexNet(BasicModule):
         super(AlexNet, self).__init__()
 
         self.model_name = 'alexnet'
-
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-        )
-        self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes),
-        )
-
+        self.model = models.AlexNet(pretainer = Ture)
+        self.model.num_classes = num_classes 
+	
+	num_fts = self.model.classifier[6].in_features
+	self.model.classifier[6] = nn.Linear(num_fts, num_classes)
+ 
     def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), 256 * 6 * 6)
-        x = self.classifier(x)
-        return x
+	return self.model(x)
+
+    def get_optimizer(self, lr, weight_decay):
+        return Adam(self.model.parameters(), lr, weight_decay)
